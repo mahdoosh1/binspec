0f67e2f8af94ef1deeb1047df6cc1b8d1993b194
mahdoosh1 <mahdiyadiilavi1@gmail.com>
2026-07-28 20:43:23 +0330

    some things

--- tar.rs
+++ tar.rs
@@ -1,4 +1,4 @@
-spec TarString(size: Int) {
+spec TarString(size: USIZE) {
     // [to_be_called; how_many_times]
     // [type; integer]
     // [|| TypeThatHasParameters(2); 5] is allowed
@@ -7,14 +7,26 @@ spec TarString(size: Int) {
     // call a type means -> read and return value of that type
     hide let end = U8();
     assert!(end == 0 || end == b' '); // not rust's assert
-    let text = for i in string { // in this language for returns Option, None if broke without a value or if loop finished, Some if broke with a value
-        if i != 0 && i != b' ' {break string}
-    };
+    hide let end = string.find(0).or(string.find(b' '));
+    let text = end.and_then(|index| string[..index]);
 }
 
-spec Oct(size = 8) { // implicitly of type Int
+spec Oct(size: USIZE = 8) {
     hide let digits = TarString(size).text;
-    let value = digits.and_then((digits) => {parse_int(digits, 8)});
+    let value: Option<U64> = digits.and_then(|digits| parse(digits, 8));
+}
+
+spec UStar {
+    let ustar_indicator = TarString(6).text;
+    assert(ustar_indicator == b"ustar");
+    let ustar_version = TarString(2).text;
+    assert(ustar.ustar_version == b"00", "This file uses a newer version of UStar that is not yet supported");
+    let owner_user_name = TarString(32).text;
+    let owner_group_name = TarString(32).text;
+    let device_major_number = Oct().value;
+    let device_minor_number = Oct().value;
+    let filename_prefix = TarString(155).text;
+    let unspecified = [U8; 12];
 }
 
 spec Header() {
@@ -26,7 +38,7 @@ spec Header() {
     let last_modified = Oct(12).value;
     let check_sum = Oct();
     enum FileType: U8 {
-        b'0' | 0 => NormalFile,
+        b'0' => NormalFile,
         b'1' => HardLink,
         b'2' => SymbolicLink,
         b'3' => CharacterSpecial,
@@ -36,25 +48,15 @@ spec Header() {
         b'7' => ContiguousFile,
         b'g' => GlobalExtendedHeaderWithMetadata,
         b'x' => ExtendedHeaderWithMetadataForTheNextFileInTheArchive,
-        b'A'..=b'Z' as a_to_z => VendorSpecificExtention(a_to_z),
-        rest => Reserved(rest)
+        b'A'..=b'Z' => VendorSpecificExtention,
+        rest => error("Reserved filetype used: {rest}")
     }
     let file_type = FileType();
-    assert(file_type != FileType::Reserved, "Reserved filetype used");
     let name_of_linked_file = TarString(100).text;
     let unused = Try( // Try will use the first type, if it fails, it uses the second type
-        spec { // anonymous spec
-            let ustar_indicator = TarString(6).text;
-            let ustar_version = TarString(2).text;
-            let owner_user_name = TarString(32).text;
-            let owner_group_name = TarString(32).text;
-            let device_major_number = Oct().value;
-            let device_minor_number = Oct().value;
-            let filename_prefix = TarString(155).text;
-            let unspecified = [U8; 12];
-        },
+        Ustar,
         [U8; 255]
-    ); // Try returns an enum with variants `Ok(type)` or `Fallback(type, error)`.
+    ); // Try returns an enum with variants `Ok(type)` or `Fallback(type, error)` or it raises an error if fallback type didn't work.
     // `value as type` -> take `value` as unparsed bytes and parse them as `type`
     assert(
         sum(self as [U8])
@@ -67,7 +69,7 @@ spec Header() {
             if ustar.ustar_indicator != b"ustar" {
                 break file_path_and_name
             }
-            assert(ustar.ustar_version == b"00", "This file uses a newer version of UStar that is not yet supported");
+            
             uStar.filename_prefix + file_path_and_name
         }
         Fallback => {
@@ -81,5 +83,5 @@ spec TarEntry() {
     let file_data = [U8; Header.FileSize];
 }
 spec File() {
-    let entries = [[0; Header.size]? TarEntry];
+    let entries = [[U8 = 0; Header.size]? TarEntry];
 }
